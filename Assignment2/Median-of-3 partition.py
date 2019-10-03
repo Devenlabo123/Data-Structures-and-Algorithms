6e7d877406c649cc5711c33c0cce71ad2f172b39
Deven Labovitch <47432806+Devenlabo123@users.noreply.github.com>
2019-10-02 21:59:09 -0400

    Create Median-of-3 partition.py

---
+++ Assignment2/Median-of-3 partition.py
@@ -0,0 +1,41 @@
+#########    Median-of-3 partition    ##########
+
+import random
+import statistics
+
+
+def quicksort(arr, start, end):
+    if start < end:
+        q = partition(arr, start, end)
+        quicksort(arr, start, q - 1)
+        quicksort(arr, q + 1, end)
+
+
+def chose_median(arr,start,end):
+    rand_values = [random.choice(arr[start:end + 1]), random.choice(arr[start:end + 1]), random.choice(arr[start:end + 1])]
+    pivot_value = statistics.median(rand_values)
+    return pivot_value
+    
+    
+def partition(arr, start, end):
+    rand_values = [random.choice(arr[start:end + 1]), random.choice(arr[start:end + 1]), random.choice(arr[start:end + 1])]
+    pivot_value = statistics.median(rand_values)
+    pivot_index = arr.index(pivot_value)
+    
+    arr[pivot_index], arr[end] = arr[end], arr[pivot_index]
+    
+    
+    i = start - 1
+    
+    for j in range(start, end):
+        if arr[j] <= pivot_value:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[end] = arr[end], arr[i+1]
+    return i + 1
+
+arr = [12, 7, 15, 3, 7, 8, 19, 2] 
+quicksort(arr, 0, len(arr) - 1) 
+for i in range(len(arr)): 
+    print (str(arr[i]) + ' ')
